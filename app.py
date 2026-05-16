6589fb2d2abf64c7b0a9d2ef3d837eee8be9294e
Rawish007 <134200704+Rawish007@users.noreply.github.com>
2026-05-16 11:27:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-dashboard/
-│
-├── app.py
-├── templates/
-│   ├── dashboard.html
-│
-├── static/
-│   ├── style.css
-│
-├── database.db
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
